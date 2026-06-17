9eaefaaa84b5cf2ebddcd961f59c028277c0529a
Salik Khan <contact@salikkhan.com>
2026-06-17 15:30:00 +0530

    fix pre-save hook, async hooks dont use next callback
    Co-authored-by: Salik Khan <contact@salikkhan.com>

--- models/User.js
+++ models/User.js
@@ -56,10 +56,9 @@ const userSchema = new mongoose.Schema({
   createdAt: { type: Date, default: Date.now },
 });
 
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
   this.password = await hash(this.password, 10);
-  next();
 });
 
 userSchema.methods.comparePassword = async function (candidate) {
